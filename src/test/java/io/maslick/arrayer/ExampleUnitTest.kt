748fa1e30e0ffffa73fe62e7c1f72d9d86ae7f7e
Pavel Maslov <pavel.maslov@ijs.si>
2018-03-28 11:23:59 +0200

    initial commit

---
+++ src/test/java/io/maslick/arrayer/ExampleUnitTest.kt
@@ -0,0 +1,134 @@
+package si.ijs.heartman.iotool.arband
+
+import com.github.davidmoten.rx.Transformers
+import io.maslick.arrayer.Helper.randomInteger
+import org.junit.Assert
+import org.junit.Test
+import rx.Observable
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.roundToInt
+
+
+class ExampleUnitTest {
+
+    val window10s = 10000L
+
+    @Test
+    fun calcCalories() {
+        val data = createTestDataArray(seconds = 60*60, freq = 100)
+        println("data size: ${data.size}")
+
+        val list = Observable
+                .from(data)
+                .compose(groupDataInWindows(window10s))
+                .map { it.map { it.ee!! }.average() }
+                .toList().toBlocking().single().toList()
+
+        println("list size: ${list.size}")
+
+        val mass = 70
+        val calories = list.map { it!! * mass / (3600 / (window10s / 1000)) }.sum().roundToInt()
+
+        println("Burnt: $calories kkal")
+    }
+
+    @Test
+    fun saveToFile() {
+        val data = createTestDataArray(seconds = 60, freq = 15)
+        File("60sec15Hz.txt").printWriter().use { out ->
+            data.forEach {
+                out.println("${it.timestamp} ${it.ee}")
+            }
+        }
+    }
+
+    @Test
+    fun compareAsyncWithSync() {
+        val data = createTestDataArray(seconds = 6000, freq = 45)
+        val groupedSync = data.groupInWindows(window10s, {it.timestamp!!})
+
+        val groupedAvg = Observable.from(groupedSync)
+                .map { it.map { it.ee!! }.average() }
+                .toList()
+                .toBlocking()
+                .single()
+                .toList()
+
+        val list = Observable.from(data)
+                .compose(groupDataInWindows(window10s))
+                .map { it.map { it.ee!! }.average() }
+                .toList()
+                .toBlocking()
+                .single()
+                .toList()
+
+
+        Assert.assertEquals(list, groupedAvg)
+    }
+
+    @Test
+    fun hoho() {
+        val data = createTestDataArray(seconds = 12, freq = 2)
+        val groupedSync = data.groupInWindows(window10s, {it.timestamp!!})
+        groupedSync.forEach { println(it) }
+    }
+
+
+    private fun createTestDataArray(seconds: Int, freq: Int): List<Data> {
+        val list = mutableListOf<Data>()
+        val currentTime = System.currentTimeMillis()
+        val delta = (1000.0/freq).toInt()
+
+        for (i in 0 until seconds*freq)
+            list.add(Data(randomInteger(2, 10) * 1.0 + randomInteger(1,10)/10.0, currentTime + i*delta))
+
+        return list.toList()
+    }
+
+    private fun groupDataInWindows(window: Long): Observable.Transformer<Data, List<Data>> {
+        return Transformers.toListWhile<Data> { data, i -> data.isEmpty() || i.timestamp!! - data[0].timestamp!! < window }
+    }
+
+    private fun List<Double>.average(): Double? {
+        return this.sum()/this.size
+    }
+
+    private fun formatDate(timestamp: Long?): String {
+        val formatter = SimpleDateFormat("HH:mm:ss")
+        return formatter.format(Date(timestamp!!))
+    }
+
+    private fun List<Data>.groupInWindows(window: Long): List<List<Data>> {
+        val ret = mutableListOf<List<Data>>()
+        var bucket = mutableListOf<Data>()
+        this.forEach { i:Data ->
+            if (bucket.isEmpty() || i.timestamp!! - bucket[0].timestamp!! < window)
+                bucket.add(i)
+            else {
+                ret.add(bucket)
+                bucket = mutableListOf()
+            }
+        }
+        if (bucket.isNotEmpty()) ret.add(bucket)
+        return ret.toList()
+    }
+
+    private fun <T> List<T>.groupInWindows(window: Long, lambda: (T) -> Long): List<List<T>> {
+        val ret = mutableListOf<List<T>>()
+        var bucket = mutableListOf<T>()
+        this.forEach { i ->
+            if (bucket.isEmpty() || lambda.invoke(i) - lambda.invoke(bucket[0]) < window)
+                bucket.add(i)
+            else {
+                ret.add(bucket)
+                bucket = mutableListOf()
+            }
+        }
+        if (bucket.isNotEmpty()) ret.add(bucket)
+        return ret.toList()
+    }
+}
+
+data class Data(var ee: Double? = null, var timestamp: Long? = null)
---
+++ src/test/java/io/maslick/arrayer/Helper.java
@@ -0,0 +1,33 @@
+package io.maslick.arrayer;
+
+
+/**
+ * Created by maslick on 4.1.2017.
+ */
+
+import java.util.Random;
+
+public final class Helper {
+
+    public static int randomInteger(int min, int max) {
+        return min + (int) (Math.random() * ((max - min) + 1));
+    }
+
+    public static String randomString(int length) {
+        String letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String numbers = "0123456789";
+
+        Random r = new Random();
+        String alphabet = letters + numbers;
+        String out = "";
+        for (int i = 0; i < length; i++) {
+            out += alphabet.charAt(r.nextInt(alphabet.length()));
+        }
+
+        return out;
+    }
+
+    public static String randomString() {
+        return randomString(5);
+    }
+}
